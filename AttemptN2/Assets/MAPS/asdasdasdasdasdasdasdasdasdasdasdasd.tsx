8cfb68a6d2331b4a92ee2f067c4d8c1c07efb3ad
Saber74 <nizarberserker@gmail.com>
2019-02-03 12:31:56 -0500

    organized syuff

--- AttemptN2/Assets/MAPS/asdasdasdasdasdasdasdasdasdasdasdasd.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="limbusdev_fwm-world" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
- <image source="ART/Pack2/FWM-Assets-Package/limbusdev_fwm-world.png" trans="ff00ff" width="1024" height="1024"/>
-</tileset>
