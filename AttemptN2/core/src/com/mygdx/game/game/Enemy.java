09f95fa0b8182e24a39156eb5a795442c891c03a
Saber74 <nizarberserker@gmail.com>
2019-02-09 21:17:53 -0500

    finalized enemy's death sounds

--- AttemptN2/core/src/com/mygdx/game/game/Enemy.java
+++ AttemptN2/core/src/com/mygdx/game/game/Enemy.java
@@ -7,11 +7,14 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.awt.*;
+import java.util.Random;
 
 
 public class Enemy {
     Sound die= Gdx.audio.newSound(Gdx.files.internal("Assets/Sound/kill.mp3"));
+    Sound die2= Gdx.audio.newSound(Gdx.files.internal("Assets/Sound/kill3.mp3"));
     Sound bomb=Gdx.audio.newSound(Gdx.files.internal("Assets/Sound/kill2.mp3"));
+    Sound bomb2=Gdx.audio.newSound(Gdx.files.internal("Assets/Sound/kill4.mp3"));
     public static int x;
     public static int y;
     public static int enemyType;
@@ -60,12 +63,10 @@ public class Enemy {
     }
 
     public boolean isCollidingWith(Bullet bullet) {
-        die.play();
         return bullet.getRect().intersects(this.getRect()) && !this.isDead();
     }
 
     public boolean isCollidingWith(SpiritBomb spiritBomb) {
-        bomb.play();
         return spiritBomb.getRect().intersects(this.getRect()) && !this.isDead();
     }
 
@@ -78,6 +79,20 @@ public class Enemy {
     }
 
     public void setDead(boolean dead) {
+        Random rand = new Random();
+        int n = rand.nextInt(5);
+        if(n==1){
+            bomb.play();
+        }
+        else if (n==2){
+            bomb2.play();
+        }
+        else if(n==3){
+            die.play();
+        }
+        else if(n==4){
+            die2.play();
+        }
         this.dead = dead;
     }
 
